7ffa6f291d746b4c1a6657dc9cbdcec8cd2b8edb
Svyatoslav <sergeyevsvyatoslav@yandex.ru>
2022-02-18 00:56:38 +0300

    ObjectMapper added

--- src/main/java/tech/sergeyev/telrostesttask/api/security/AuthController.java
+++ src/main/java/tech/sergeyev/telrostesttask/api/security/AuthController.java
@@ -1,50 +1,50 @@
 package tech.sergeyev.telrostesttask.api.security;
 
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tech.sergeyev.telrostesttask.payload.LoginDto;
 import tech.sergeyev.telrostesttask.payload.RegistrationDto;
-import tech.sergeyev.telrostesttask.persistence.dao.PersonRepository;
 import tech.sergeyev.telrostesttask.persistence.model.Person;
 import tech.sergeyev.telrostesttask.service.person.PersonServiceImpl;
 
-import java.net.URI;
-import java.time.LocalDate;
-
 @RestController
 @RequestMapping
-@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@FieldDefaults(
+        level = AccessLevel.PRIVATE,
+        makeFinal = true
+)
 public class AuthController {
     private static final Logger LOGGER = LoggerFactory.getLogger(AuthController.class);
 
     AuthenticationManager authenticationManager;
     PersonServiceImpl personService;
-    PasswordEncoder passwordEncoder;
+    ObjectMapper mapper;
 
     public AuthController(AuthenticationManager authenticationManager,
                           PersonServiceImpl personService,
-                          PasswordEncoder passwordEncoder) {
+                          ObjectMapper mapper) {
         this.authenticationManager = authenticationManager;
         this.personService = personService;
-        this.passwordEncoder = passwordEncoder;
+        this.mapper = mapper;
     }
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginDto data) {
+        LOGGER.info("Login request received: username={}", data.getLogin());
         Authentication auth = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
                         data.getLogin(),
@@ -56,22 +56,14 @@ public class AuthController {
     }
 
     @PostMapping("/signup")
-    public ResponseEntity<?> registration(@RequestBody RegistrationDto data) {
+    public ResponseEntity<?> registration(@RequestBody RegistrationDto data) throws JsonMappingException {
         if (personService.existsPersonByLogin(data.getLogin())) {
             return new ResponseEntity<>("Username is already exists", HttpStatus.FORBIDDEN);
         }
         Person user = new Person();
-        user.setName(data.getName());
-        user.setLastname(data.getLastname());
-        user.setPatronymic(data.getPatronymic());
-        user.setDateOfBirth(LocalDate.parse(data.getDateOfBirth()));
-        user.setEmail(data.getEmail());
-        user.setPhone(data.getPhone());
-        user.setLogin(data.getLogin());
-        user.setPassword(passwordEncoder.encode(data.getPassword()));
+        mapper.updateValue(user, data);
 
         personService.save(user);
-
-        return new ResponseEntity<>("You have successfully registered", HttpStatus.CREATED);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
     }
 }

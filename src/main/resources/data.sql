76525a6ddb89b1be60c8ac2f798d28ea66004259
Svyatoslav <sergeyevsvyatoslav@yandex.ru>
2022-02-18 02:16:47 +0300

    added decoded password

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
 insert into person values
-(0, '', '', '', NULL, '', '', 'admin', '$2a$12$OZcmXqorgYOopvUdjy6OvOS0C2I5D4TcncrglW7nCqYtrc673vldK')
+(0, '', '', '', NULL, '', '', 'admin', '$2a$12$OZcmXqorgYOopvUdjy6OvOS0C2I5D4TcncrglW7nCqYtrc673vldK') --login:admin, pass:admin
 on conflict do nothing ;
